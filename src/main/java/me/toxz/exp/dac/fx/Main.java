1b2ae8810ea64540c753cf210861ddbca134f26c
Carlos <toxz@foxmail.com>
2016-01-04 09:42:59 +0800

    add main test fx sence

---
+++ src/main/java/me/toxz/exp/dac/fx/Main.java
@@ -0,0 +1,26 @@
+package me.toxz.exp.dac.fx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Carlos on 1/4/16.
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.show();
+    }
+}
+

9489e59f599e7701cd6049e15354ec69896f4e1f
Carlos <toxz@foxmail.com>
2016-01-06 16:18:24 +0800

    add Object Create Dialog

--- src/main/java/me/toxz/exp/dac/fx/CenterSceneController.java
+++ src/main/java/me/toxz/exp/dac/fx/CenterSceneController.java
@@ -21,9 +21,11 @@ package me.toxz.exp.dac.fx;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import me.toxz.exp.dac.data.DatabaseHelper;
 import me.toxz.exp.dac.data.model.*;
+import me.toxz.exp.dac.fx.animation.ShakeTransition;
 
 import java.io.IOException;
 import java.net.URL;
@@ -31,6 +33,7 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -89,13 +92,45 @@ public class CenterSceneController implements Initializable {
     }
 
     public void onCreateObject(ActionEvent actionEvent) {
-        TextInputDialog dialog = new TextInputDialog();
+        final TextInputDialog dialog = new TextInputDialog();
         dialog.setTitle("New object");
-        dialog.setContentText("Object path");
+        dialog.setContentText("Path");
+        dialog.setHeaderText("Please input information:");
+
+        final TextField textField = dialog.getEditor();
+
+        final Button okButton = (Button) dialog.getDialogPane().lookupButton(ButtonType.OK);
+        final Consumer<Node> shake = node -> new ShakeTransition(node, event -> {
+        }).playFromStart();
+        okButton.addEventFilter(ActionEvent.ACTION, ae -> {
+            String path = textField.getText();
+
+
+            if (path == null || path.length() == 0) {
+                shake.accept(textField);
+                dialog.setHeaderText("Please input the path!");
+                ae.consume();
+            } else try {
+                if (!DatabaseHelper.getMObjectDao().queryForMatching(new MObject(path)).isEmpty()) {
+                    shake.accept(textField);
+                    dialog.setHeaderText("Object path exist! Try another one.");
+                    ae.consume();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                dialog.setHeaderText("Error! " + e.getMessage());
+            }
+        });
+
         Optional<String> result = dialog.showAndWait();
 
         result.ifPresent(s -> {
-
+            try {
+                DatabaseHelper.getMObjectDao().create(new MObject(s));
+                //TODO refresh table
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         });
     }
 }

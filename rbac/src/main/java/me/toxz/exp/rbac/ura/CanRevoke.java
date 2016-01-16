50e2258886763e9133df12e248fc7afa00d97767
Carlos <toxz@foxmail.com>
2016-01-16 21:10:39 +0800

    store revoke

--- rbac/src/main/java/me/toxz/exp/rbac/ura/CanRevoke.java
+++ rbac/src/main/java/me/toxz/exp/rbac/ura/CanRevoke.java
@@ -1,13 +1,20 @@
 package me.toxz.exp.rbac.ura;
 
 import com.google.common.collect.Range;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
 import me.toxz.exp.rbac.Role;
+import me.toxz.exp.rbac.data.RoleRangeType;
 
 /**
  * Created by Carlos on 2016/1/16.
  */
+@DatabaseTable
 public class CanRevoke {
+    @DatabaseField(generatedId = true) private int _id;
+    @DatabaseField(canBeNull = false, foreign = true, foreignAutoRefresh = true)
     private Role operator;
+    @DatabaseField(persisterClass = RoleRangeType.class)
     private Range<Role> range;
 
     public CanRevoke(Role operator, Range<Role> range) {

7142fe6c763e5ede1c1ef0dd806fca6904a5e3a4
Carlos <toxz@foxmail.com>
2016-01-16 21:47:43 +0800

    store condition

--- rbac/src/main/java/me/toxz/exp/rbac/ura/Condition.java
+++ rbac/src/main/java/me/toxz/exp/rbac/ura/Condition.java
@@ -1,10 +1,20 @@
 package me.toxz.exp.rbac.ura;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+import me.toxz.exp.rbac.Role;
 import me.toxz.exp.rbac.User;
 
 /**
  * Created by Carlos on 2016/1/16.
  */
-public interface Condition {
-    boolean can(User user);
+@DatabaseTable
+public class Condition {
+    @DatabaseField(canBeNull = false, foreign = true, foreignAutoRefresh = true)
+    private Role requireRole;
+
+    public boolean can(User user) {
+        //TODO implement
+        return false;
+    }
 }

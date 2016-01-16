feff7fc963a89ce3a2bdd19371e118b79fee7659
Carlos <toxz@foxmail.com>
2016-01-16 20:35:12 +0800

    store canAssign

--- rbac/src/main/java/me/toxz/exp/rbac/ura/CanAssign.java
+++ rbac/src/main/java/me/toxz/exp/rbac/ura/CanAssign.java
@@ -1,16 +1,24 @@
 package me.toxz.exp.rbac.ura;
 
 import com.google.common.collect.Range;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
 import me.toxz.exp.rbac.Role;
+import me.toxz.exp.rbac.data.RangeType;
 
 /**
  * URA97
  * <p>
  * Created by Carlos on 2016/1/16.
  */
+@DatabaseTable
 public class CanAssign {
+    @DatabaseField(canBeNull = false, foreign = true, foreignAutoRefresh = true)
     private Role operator;
+    @DatabaseField(canBeNull = false, foreign = true, foreignAutoRefresh = true)
     private Condition condition;
+
+    @DatabaseField(persisterClass = RangeType.class)
     private Range<Role> range;
 
     public CanAssign(Condition condition, Role operator, Range<Role> range) {
